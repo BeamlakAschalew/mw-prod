9998e23b09fa4c397f60dfe8866423c09f46bdf0
Beamlak Aschalew <birrletej12@gmail.com>
2023-12-18 20:24:10 +0300

    update

--- src/providers/sources/gomovies/index.ts
+++ src/providers/sources/gomovies/index.ts
@@ -36,20 +36,18 @@ export const goMoviesScraper = makeSourcerer({
 
     const numberPattern = /\d+/;
 
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < mediaData.length; i++) {
-      // Use the match method to find the first match in the string
+    for (let i = 0; i < mediaData.length; i += 1) {
       const matchResult: RegExpMatchArray | null = mediaData[i].year.match(numberPattern);
-      // Check if a match is found and extract the number
       if (matchResult) {
         const extractedNumber: number = parseInt(matchResult[0], 10);
-        console.log(extractedNumber); // Output: 9
-      } else {
-        console.log('No number found in the string');
+        console.log(extractedNumber);
+        mediaData[i].year = extractedNumber.toString();
       }
     }
 
-    const targetMedia = mediaData.find((m) => m.name === ctx.media.title);
+    const targetMedia = mediaData.find(
+      (m) => m.name === ctx.media.title && m.year === ctx.media.numberOfSeasons.toString(),
+    );
     if (!targetMedia?.path) throw new NotFoundError('Media not found');
 
     // Example movie path: /movie/watch-{slug}-{id}
