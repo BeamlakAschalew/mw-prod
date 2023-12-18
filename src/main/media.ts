b3162432e9024ba5ecaf6013c79c54f2eb504749
Beamlak Aschalew <birrletej12@gmail.com>
2023-12-18 19:32:04 +0300

    init

---
+++ src/main/media.ts
@@ -0,0 +1,28 @@
+export type CommonMedia = {
+  title: string;
+  releaseYear: number;
+  imdbId?: string;
+  tmdbId: string;
+};
+
+export type MediaTypes = 'show' | 'movie';
+
+export type ShowMedia = CommonMedia & {
+  type: 'show';
+  episode: {
+    number: number;
+    tmdbId: string;
+  };
+  season: {
+    number: number;
+    tmdbId: string;
+  };
+
+  numberOfSeasons: number;
+};
+
+export type MovieMedia = CommonMedia & {
+  type: 'movie';
+};
+
+export type ScrapeMedia = ShowMedia | MovieMedia;
