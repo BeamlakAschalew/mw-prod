cb69d91ab9125e913cd8a8cc0b6cca3feecdb8af
Beamlak Aschalew <birrletej12@gmail.com>
2024-01-06 23:45:33 +0300

    deleted dev-cli

--- src/dev-cli/logging.ts
+++
@@ -1,5 +0,0 @@
-import { inspect } from 'node:util';
-
-export function logDeepObject(object: Record<any, any>) {
-  console.log(inspect(object, { showHidden: false, depth: null, colors: true }));
-}
