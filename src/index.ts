6de3a5ea51b3bca204ec0f924c4d1a0ebb473252
Beamlak Aschalew <birrletej12@gmail.com>
2024-03-15 17:29:59 +0300

    v2.2.3 migrate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { getDecryptionKeys } from './providers/embeds/vidplay/common';
-
 export type { EmbedOutput, SourcererOutput } from '@/providers/base';
 export type { Stream, StreamFile, FileBasedStream, HlsBasedStream, Qualities } from '@/providers/streams';
 export type { Fetcher, DefaultedFetcherOptions, FetcherOptions, FetcherResponse } from '@/fetchers/types';
