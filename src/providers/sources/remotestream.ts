e218513f6c50e82933d54303f8c14af184d0be55
Beamlak Aschalew <birrletej12@gmail.com>
2024-03-31 10:25:55 +0300

    Fix ShowBox

--- src/providers/sources/remotestream.ts
+++ src/providers/sources/remotestream.ts
@@ -10,7 +10,8 @@ const referer = 'https://remotestre.am/';
 export const remotestreamScraper = makeSourcerer({
   id: 'remotestream',
   name: 'Remote Stream',
-  rank: 55,
+  disabled: true,
+  rank: 20,
   flags: [flags.CORS_ALLOWED],
   async scrapeShow(ctx) {
     const seasonNumber = ctx.media.season.number;
